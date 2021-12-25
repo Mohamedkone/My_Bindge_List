28ebf63a52acb1e3b10f2c2680aae543d4594fa1
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2021-12-24 20:35:55 -0500

    completed Tv container on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,9 @@ const Home = () =>{
 //   const search1 = document.getElementById('search-input');
 let movieList
 const [MovieInfo, SetMovieInfo] = useState()
-const [Path, SetPath] = useState()
+const [TvInfo, SetTvInfo] = useState()
+const [MovieLength, SetMovieLength] = useState()
+const [TvLength, SetTvLength] = useState()
 
     useEffect(()=>{
         axios.get(
@@ -18,17 +20,29 @@ const [Path, SetPath] = useState()
         ).then(res =>{
             console.log(res.data.results[0].poster_path)
             SetMovieInfo(res.data.results)
-            SetPath(10)
+            SetMovieLength(10)
         
+        })
+
+    },[])
+    useEffect(()=>{
+        axios.get(
+            `${process.env.REACT_APP_DOMAIN}/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+            
+        ).then(test =>{
+            console.log(test.data.results[0].poster_path)
+            SetTvInfo(test.data.results)
+            SetTvLength(10)     
         })
 
     },[])
     console.log(MovieInfo)
     let length = 1
     let Cards = []
+    let Cards2 =[]
     // console.log(MovieInfo[0].poster_path)
 
-          for (let i = 0; i < Path; i++) {
+          for (let i = 0; i < MovieLength; i++) {
               Cards.push(<div className="card">
                 
                 <div className="info">
@@ -37,6 +51,17 @@ const [Path, SetPath] = useState()
                 </div>
                 <img src={`${process.env.REACT_APP_IMG_PATH}${MovieInfo[i].poster_path}`} />
             </div>)
+          }
+            for (let i = 0; i < TvLength; i++) {
+                Cards2.push(<div className="card">
+                
+                <div class="info">
+                    <h3>{TvInfo[i].name}</h3>
+                    <p class="rl-date"> Aired on: {TvInfo[i].first_air_date}</p>
+                </div>
+                <img src={`${process.env.REACT_APP_IMG_PATH}${TvInfo[i].poster_path}`} />
+            </div>)
+            }
 
             // if (param[i].poster_path == null) {
             //   addMovie.innerHTML += `<img src='https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg'>`;
@@ -44,7 +69,7 @@ const [Path, SetPath] = useState()
             //   addMovie.innerHTML += `<img src=${IMAGE_BASE_PATH}${param[i].poster_path}>`;
             // }
             // movieList.append(addMovie);
-          }
+          
         
       
      
@@ -61,7 +86,7 @@ return(
         </div>
         <h2>More Tv Show to Bindge...</h2>
         <div className="container2">
-        
+            {Cards2}
         </div>
   </section>
 )
