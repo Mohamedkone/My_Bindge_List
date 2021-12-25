5a691d8d702f011cec0e62700b4fd4db9bcbb17f
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2021-12-24 20:52:49 -0500

    fixed padding and made container responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import NavBar from './components/NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './Style/App.css';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <NavBar />
+        <div className='components'>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+        </div>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
