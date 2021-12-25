39a61ae4cca6d240100076a1dd7a9508e9aea1a5
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2021-12-24 20:00:11 -0500

    completed movie component for home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "../Style/App.css"
+
+
+const Home = () =>{
+
+//     const button1 = document.getElementById('searchHome');
+//   const search1 = document.getElementById('search-input');
+let movieList
+const [MovieInfo, SetMovieInfo] = useState()
+const [Path, SetPath] = useState()
+
+    useEffect(()=>{
+        axios.get(
+            `${process.env.REACT_APP_DOMAIN}/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+            
+        ).then(res =>{
+            console.log(res.data.results[0].poster_path)
+            SetMovieInfo(res.data.results)
+            SetPath(10)
+        
+        })
+
+    },[])
+    console.log(MovieInfo)
+    let length = 1
+    let Cards = []
+    // console.log(MovieInfo[0].poster_path)
+
+          for (let i = 0; i < Path; i++) {
+              Cards.push(<div className="card">
+                
+                <div className="info">
+                    <h3>{MovieInfo[i].title}</h3>
+                    <p className="rl-date"> Released on: {MovieInfo[i].release_date}</p>
+                </div>
+                <img src={`${process.env.REACT_APP_IMG_PATH}${MovieInfo[i].poster_path}`} />
+            </div>)
+
+            // if (param[i].poster_path == null) {
+            //   addMovie.innerHTML += `<img src='https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg'>`;
+            // } else {
+            //   addMovie.innerHTML += `<img src=${IMAGE_BASE_PATH}${param[i].poster_path}>`;
+            // }
+            // movieList.append(addMovie);
+          }
+        
+      
+     
+    // const response2 = await axios.get(
+    //   `${process.env.DOMAIN}/discover/tv?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+    // );
+
+
+return(
+    <section className="movie-list">
+        <h2>Recent Movies To Bindge...</h2>
+        <div className="container1">
+            {Cards}
+        </div>
+        <h2>More Tv Show to Bindge...</h2>
+        <div className="container2">
+        
+        </div>
+  </section>
+)
+}
+
+export default Home
